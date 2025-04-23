8dcfe7557eb327bbe809342a7f847d2d7e80e7e1
Knok98 <petr.masa30@gmail.com>
2025-04-23 14:59:47 +0200

    init ftp services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { XmlToCsvModule } from './xml-to-csv/xml-to-csv.module';
-import { FtpConnectService } from './ftp-connect/ftp-connect.service';
 import { FtpConnectModule } from './ftp-connect/ftp-connect.module';
 
 @Module({
   imports: [XmlToCsvModule, FtpConnectModule],
   controllers: [],
-  providers: [FtpConnectService],
+  providers: [],
 })
 export class AppModule {}
--- src/ftp-connect/ftp-connect.module.ts
+++ src/ftp-connect/ftp-connect.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FtpConnectController } from './ftp-connect.controller';
+import { FtpConnectService } from './ftp-connect.service';
 
 @Module({
+  providers: [FtpConnectService],
   controllers: [FtpConnectController]
 })
 export class FtpConnectModule {}
