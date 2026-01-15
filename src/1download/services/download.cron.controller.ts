b390d0287f828b44a316d6bebfa831b65ed18a84
Knok98 <113551380+Knok98@users.noreply.github.com>
2026-01-15 15:32:55 +0100

    Log completion message for cron download operation
    Added a console log to indicate completion of the cron download operation.

--- src/1download/services/download.cron.controller.ts
+++ src/1download/services/download.cron.controller.ts
@@ -11,5 +11,6 @@ export class FileDownloadCronService {
   @Cron('50 2,14 * * *')
   async handleCron() {
     await this.fileDownloadService.downloadFile();
+    console.log("cron operation - download done");
   }
 }
